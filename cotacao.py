2e678a6b7c9f7abcbfacfb4d4f1063156e6e045e
David Costa <zevid.dev@outlook.com>
2023-01-16 10:49:24 -0300

    ✨ feat:calculate new values and save in new table

--- cotacao.py
+++ cotacao.py
@@ -1,8 +1,8 @@
-# Passo 1: Acessar o navegador
 import pyautogui
 import time
 import pyperclip
 import pandas as pd
+
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,7 +15,6 @@ chrome.get("https://www.google.com/")
 chrome.implicitly_wait(0.5)
 
 
-# Passo 2: Pesquisar a cotação da moeda desejada
 pyperclip.copy('cotação do dólar')
 pyautogui.hotkey('ctrl', 'v')
 time.sleep(0.5)
@@ -27,7 +26,8 @@ price_dolar = chrome.find_element(
 
 file_products = pd.read_excel('Produtos.xlsx')
 
-file_products.loc[file_products['Moeda'] == 'Dólar', 'Cotação'] = price_dolar
+file_products.loc[file_products['Moeda'] == 'Dólar', 'Cotação'] = float(price_dolar)
+
 
 chrome.get("https://www.google.com/")
 
@@ -40,16 +40,18 @@ price_euro = chrome.find_element(
     By.XPATH,
     '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 
-file_products.loc[file_products['Moeda'] == 'Euro', 'Cotação'] = price_euro
+file_products.loc[file_products['Moeda'] == 'Euro', 'Cotação'] = float(price_euro)
 
 chrome.get('https://www.melhorcambio.com/ouro-hoje#:~:text=O%20valor%20do%20grama%20do,%C3%A9%20de%20car%C3%A1ter%20exclusivamente%20informativo.')
+
 price_gold = chrome.find_element(
     By.XPATH,
     '//*[@id="comercial"]'
 ).get_attribute('value')
-file_products.loc[file_products['Moeda'] == 'Ouro', 'Cotação'] == price_gold
+price_gold = price_gold.replace(',','.')
 
-# Passo 3: Enviar esse dado da cotação para a tabela
-# Passo 4: Calcular os valores de venda com base na cotação atual
+file_products.loc[file_products['Moeda'] == 'Ouro', 'Cotação'] == float(price_gold)
+file_products['Preço de Compra'] = file_products['Preço Original'] * file_products['Cotação']
+file_products['Preço de Venda'] = file_products['Preço de Compra'] * file_products['Margem']
 
-
+file_products.to_excel('novos_produtos.xlsx', index=False)
